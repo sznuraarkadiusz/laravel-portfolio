8220f4434098ba829a4d06ed7981a88f0c1bb965
sznuraarkadiusz <arkadiusz.sznura@gmail.com>
2023-04-20 22:29:23 +0200

    install swiper.js

--- resources/views/page-templates/footer.blade.php
+++ resources/views/page-templates/footer.blade.php
@@ -1,36 +1,60 @@
 <!-- Footer-->
 <!-- Footer -->
 <footer class="text-center text-lg-start bg-light text-muted">
+    <!-- Slider main container -->
+    <div class="swiper">
+        <!-- Additional required wrapper -->
+        <div class="swiper-wrapper">
+            <!-- Slides -->
+            <div class="swiper-slide">Slide 1</div>
+            <div class="swiper-slide">Slide 2</div>
+            <div class="swiper-slide">Slide 3</div>
+            ...
+        </div>
+        <!-- If we need pagination -->
+        <div class="swiper-pagination"></div>
 
+        <!-- If we need navigation buttons -->
+        <div class="swiper-button-prev"></div>
+        <div class="swiper-button-next"></div>
+
+        <!-- If we need scrollbar -->
+        <div class="swiper-scrollbar"></div>
+    </div>
     <!-- Section: Links  -->
     <section class="py-3">
         <div class="container text-center text-md-start mt-5">
             <!-- Grid row -->
-            <div class="row mt-3">
+            <div class="row mt-2">
                 <!-- Grid column -->
-                <div class="col-md-4 col-lg-4 col-xl-4 mx-auto mb-4">
+                <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mb-6">
                     <!-- Content -->
                     <h6 class="text-uppercase fw-bold mb-4">
                         <i class="fas fa-gem me-3"></i>Arkadiusz Sznura
                     </h6>
-                    <p>
+                    <p style="text-align: justify">
                         If you are looking for a competent and honest person who is not afraid of challenges in the area of creating websites and web applications, you've come to the right place!
                     </p>
                 </div>
                 <!-- Grid column -->
 
                 <!-- Grid column -->
-                <div class="col-md-4 col-lg-4 col-xl-4 mx-auto mb-4">
+                <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mb-6">
                     <!-- Links -->
                     <h6 class="text-uppercase fw-bold mb-4">
                         Useful links
                     </h6>
+                    <div class="footer-links">
+                        <a href="" class="footer-social-link"><i class="fa-brands fa-github"></i></a>
+                        <a href="" class="footer-social-link"><i class="fa-brands fa-linkedin"></i></a>
+                        <a href="" class="footer-social-link"><i class="fa-brands fa-facebook"></i></a>
+                    </div>
 
                 </div>
                 <!-- Grid column -->
 
                 <!-- Grid column -->
-                <div class="col-md-4 col-lg-4 col-xl-4 mx-auto mb-md-0 mb-4">
+                <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-3">
                     <!-- Links -->
                     <h6 class="text-uppercase fw-bold mb-4">Contact</h6>
                     <p>
